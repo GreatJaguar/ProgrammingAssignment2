2023b314b2488d0b26fc3b5e094fd46c7f490051
GreatJaguar <harli.camposeco@yahoo.com>
2020-08-03 17:04:35 -0600

    ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,26 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  greatjaguar<- NULL
+  
+  set<- function(y){
+    x<<-y
+    greatjaguar<<-NULL
+  }
+  
+  get<-function()x
+  
+  setInverse<- function(inverse)greatjaguar<<-inverse
+  
+  getInverse<- function()greatjaguar
+  
+  list ( set = set, get = get, setInverse = setInverse, getInverse=getInverse)
 
 }
 
@@ -12,4 +29,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  greatjaguar <- x$getInverse()
+  
+  if (!is.null(greatjaguar)){
+    message("getting cached data")
+    return(greatjaguar)
+  }
+  datos<- x$get()
+  greatjaguar<-solve(datos, ...)
+  x$setInverse(greatjaguar)
+  greatjaguar
 }
